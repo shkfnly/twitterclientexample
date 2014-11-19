dbc4aec3146cd3ef4627e58bb1e0a7a880fa4de2
Ashoka Finley <ashoka.finley@gmail.com>
2014-11-18 18:56:14 -0800

    Add new test"

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -13,6 +13,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                               password_confirmation: "bar" }
     end
     assert_template 'users/new'
+   # assert_select 'div#<CSS id for error explanation>'
+   #assert_select 'div.<CSS class for field with error>'
   end
 
   test "valid sign up information" do
@@ -24,5 +26,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                           password_confirmation: "password"}
     end
     assert_template 'users/show'
+    assert_not flash.danger
   end
 end
